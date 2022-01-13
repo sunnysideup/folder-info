c053baffc67fa1232b3b93403b3296f82adaaed0
Nicolaas / Sunn Side Up <github@sunnysideup.co.nz>
2022-01-14 12:05:27 +1300

    Update MyFolderInfo.php

--- src/MyFolderInfo.php
+++ src/MyFolderInfo.php
@@ -7,49 +7,49 @@ class MyFolderInfo {
 
     protected static $data = [];
 
-    public static function LastUpdatedRaw (string $path) : int
+    public static function lastUpdatedRaw (string $path) : int
     {
         $data = self::run($path);
 
         return $data['lastUpdated'] ?? 0;
     }
 
-    public static function LastUpdatedHuman ($path) : string
+    public static function lastUpdatedHuman ($path) : string
     {
         $data = self::run($path);
 
         return date('Y-m-d H:i', self::LastUpdatedRaw($path));
     }
 
-    public static function SizeOfFilesRaw ($path) : int
+    public static function sizeOfFilesRaw ($path) : int
     {
         $data = self::run($path);
 
         return $data['size'] ?? -1;
     }
 
-    public static function SizeOfFilesHuman (string $path) : string
+    public static function sizeOfFilesHuman (string $path) : string
     {
         $data = self::run($path);
 
         return self::HumanFileSize(self::SizeOfFilesRaw($path));
     }
 
-    public static function CountOfFilesRaw (string $path) : int
+    public static function countOfFilesRaw (string $path) : int
     {
         $data = self::run($path);
 
         return $data['count'] ?? -1;
     }
 
-    public static function CountOfFilesHuman (string $path) : string
+    public static function countOfFilesHuman (string $path) : string
     {
         $data = self::run($path);
 
         return number_format(self::CountOfFilesRaw($path));
     }
 
-    public static function Run (string $path) : array
+    public static function run (string $path) : array
     {
         $path = realpath($path);
         if(! isset(self::$data[$path])) {
@@ -72,7 +72,7 @@ class MyFolderInfo {
         return self::$data[$path];
     }
 
-    public static function HumanFileSize($bytes, $decimals = 2) : string
+    public static function humanFileSize($bytes, $decimals = 2) : string
     {
         $size = array('B','kB','MB','GB','TB','PB','EB','ZB','YB');
         $factor = floor((strlen($bytes) - 1) / 3);
